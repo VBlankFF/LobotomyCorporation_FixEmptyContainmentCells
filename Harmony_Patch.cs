5d19bfaad243606fd2d5716a4b07828ca74f6d41
VBlankFF <vblankff@gmail.com>
2025-07-23 12:33:17 -0400

    Finish setting up

--- Harmony_Patch.cs
+++ Harmony_Patch.cs
@@ -9,6 +9,9 @@ namespace FixEmptyContainmentCells
     /* Current approach: if there's less abnormities than there should be given the expansion level, add more in the missing slots
      * Which method to hijack?
      * Does this break with energy corp? (probably)
+     * How to determine which abno to add?
+     * Where to get all this info from?
+     * Can you bring up an abno choice screen for this without it breaking everything? (doubtful)
      */
     public class Harmony_Patch
     {
