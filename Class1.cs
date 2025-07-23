3335335336c7560bffc3579090e1d18475c43fca
VBlankFF <vblankff@gmail.com>
2025-07-23 12:30:48 -0400

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FixEmptyContainmentCells
+{
+    /* Current approach: if there's less abnormities than there should be given the expansion level, add more in the missing slots
+     * Which method to hijack?
+     * Does this break with energy corp? (probably)
+     */
+    public class Harmony_Patch
+    {
+        public Harmony_Patch()
+        {
+
+        }
+    }
+}
