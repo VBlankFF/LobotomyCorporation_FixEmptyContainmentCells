9e8478707fb9874cea468b2cdbc1c851f1053653
VBlankFF <vblankff@gmail.com>
2025-09-01 05:21:47 -0400

    added details about Harmony

--- Harmony_Patch.cs
+++ Harmony_Patch.cs
@@ -12,32 +12,41 @@ using UnityEngine;
 namespace FixEmptyContainmentCells
 {
     /* Current approach: if there's less abnormities than there should be given the expansion level, add more in the missing slots
-     * Which method to hijack? postfix CreatureManager.LoadData():
-     * It might be used I'm not sure
-     * 
-     * Prefix SefiraPanel.Init
-     * 
-     * Does this break with energy corp? (probably)
-     * How to determine which abno to add?
-     * Where to get all this info from?
-     * Can you bring up an abno choice screen for this without it breaking everything? (doubtful)
+     * Which method to patch? Prefix SefiraPanel.Init
      */
+    // The class containing your Harmony Patches has to be named Harmony_Patch for BaseMod/LMM to load it
     public class Harmony_Patch
     {
+        // BaseMod/LMM will run the code in the constructor for the Harmony_Patch class
         public Harmony_Patch()
         {
             try
             {
+                /* You must create a HarmonyInstance using HarmonyInstance.Create. The name has to be unique.
+                The HarmonyInstance is what allows you to do Harmony patches. */
                 HarmonyInstance HInstance = HarmonyInstance.Create("VBlankFF_FixMissingContainmentUnits");
+                // To use a method as a patch you have to make it a Harmony Method first. Here I get the method by name.
                 HarmonyMethod fillEmptySlots = new HarmonyMethod(typeof(Harmony_Patch).GetMethod("FillEmptyCells"));
+                /* This is what patches the code of the game. The parameters are the method in the game you want to change, 
+                and a Prefix, a Postfix, and a Transpiler (each are HarmonyMethods). Prefixes are run before the game's code, Postfixes are run after,
+                and Transpilers are their own, more complicated thing that I won't explain here. null just means you aren't applying that kind of patch.
+                Here a Prefix is applied, and no Postfix or Transpiler is applied. */
                 HInstance.Patch(typeof(SefiraPanel).GetMethod("Init"), fillEmptySlots, null, null);
+                // This is just here for the log, it's not required for patching at all
                 UnityEngine.Debug.Log("FixMissingContainmentUnits load success");
             }
+            /* If your patching code throws an error and you do not catch it it will fail to load and the game will immediately stop loading mods. This will mess with other mods,
+            and the player will not be notified of the load issue at all unless they have LoadErrorCheck.*/
             catch (Exception e)
             {
+                // Catching the exception might prevent LoadErrorCheck from catching it, though...
+                // I think it's still worth doing because a lot of people do not have LoadErrorCheck anyway
                 UnityEngine.Debug.Log("FixMissingContainmentCells: An error has occured during load: " + e.ToString());
             }
         }
+        // All Harmony Patches (or at least Prefixes and Postfixes) must be static and probably public?
+        // This method is a Prefix for SefiraPanel.Init(SefiraEnum sefira). A Prefix can use the parameters passed to the original method if you put them in your patch and they have
+        // the same name and type.
         public static void FillEmptyCells(SefiraEnum sefira)
         {
             try
@@ -68,6 +77,7 @@ namespace FixEmptyContainmentCells
                 UnityEngine.Debug.Log("FixMissingContainmentCells: An error has occured: " + e.ToString());
             }
         }
+        // This is not a Harmony patch, it's just a method called by FillEmptyCells()
         public static int FindNumCreatures(Sefira sephirot)
         {
             // check for energy corp
@@ -115,6 +125,7 @@ namespace FixEmptyContainmentCells
                     return Math.Min(sephirot.openLevel, 4);
             }
         }
+        // This is not a Harmony patch, it's just a method called by FillEmptyCells()
         public static long GetRandomNewCreature()
         {
             List<long> newCreatures = new List<long>();
